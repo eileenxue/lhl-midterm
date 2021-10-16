bb38b85ccae1160ddae69c2253a8f9d67fe26c31
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-16 16:06:35 -0400

    db files are updated due to bugs

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,6 +3,7 @@ INSERT INTO users (name, email) VALUES ('Alice', 'alice@gmail.com');
 INSERT INTO users (name, email) VALUES ('Eileen', 'eileen@gmail.com');
 INSERT INTO users (name, email) VALUES ('Emily', 'emily@gmail.com');
 INSERT INTO users (name, email) VALUES ('yavuz', 'yavuz@gmail.com');
+INSERT INTO users (name, email) VALUES ('Mark', 'mark@gmail.com')
 
 
 
