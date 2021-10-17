0572e4b409ad3f200a90d03395d5f0bfe808870a
Emily K <emily.ke@hotmail.com>
2021-10-16 22:05:02 -0400

    last commit for calendar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,15 @@
 // Client facing scripts here
+
+isLeapYear = (year) => {
+  return (year % 4 === 0 && year % 100 !== 0 && year % 400 !==0 || (year % 100 === 0 && year % 400 === 0));
+}
+
+daysFeb = (year) => {
+  return isLeapYear(year) ?  29 : 28;
+}
+
+let calendar = document.querySelector('.calendar');
+
+generateCalendar = (month, year)  => {
+  let calenderDays = document.querySelector('.calendar-days')
+}
